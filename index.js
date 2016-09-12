f276d59e29bc32a30c3cfa92f276901132c76c27
Eduardo Lopez <eddielopez2101@gmail.com>
2016-09-12 15:58:57 -0500

    imported all js files and routed with nav nest

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Main from './lib/main'
+import { Router, Route, hashHistory } from 'react-router'
+import Nav from './lib/Nav'
+import Main from './lib/Main'
+import NewArticle from './lib/NewArticle'
+import Article from './lib/Article'
 
 render((
-  <Main name="sa-tech-voices"/>
+  <Router history={ hashHistory }>
+    <Route path="/" component={ Nav }>
+      <Route path="/main" component={ Main }/>
+      <Route path="new-article" component={ NewArticle }/>
+      <Route path="/article" component={ Article }/>
+    </Route>
+  </Router>
 ), document.getElementById('app'))
