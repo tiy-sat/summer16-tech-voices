a002c47478177f0c47206bfff84891b9e9bd3920
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-16 15:41:51 -0500

    babies first test

--- __tests__/main-test.js
+++ __tests__/main-test.js
@@ -7,10 +7,29 @@ jest.mock("../node_modules/react-google-login")
 
 describe("Main", () => {
   it("should have blogs", () => {
-    const main = TestUtils.renderIntoDocument(
+    let mainRendered = TestUtils.renderIntoDocument(
       <Main/>
     )
-    const mainEl = ReactDOM.findDOMNode(main)
-    expect(mainEl).toBe(mainEl)
+    // fake state for blogPosts
+    mainRendered.setState({
+      blogPosts: [
+         {
+          _id: "1738",
+          author: "Fetty",
+          timeStamp: "2016.09.16",
+          content: "Lorem",
+          title: "moms spaghetti",
+          user_info: {
+            user_id: "679",
+            user_name: "remys boys",
+            user_imgSrc: "remyboys.jpeg"
+          }
+        }
+      ]
+    })
+    // I want this to find an article
+    let articleEl = TestUtils.findRenderedDOMComponentWithClass(mainRendered,"article__previews")
+    console.log(articleEl);
+    expect(articleEl).toBeDefined()
   })
 })
