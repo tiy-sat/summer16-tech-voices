10507d16784242bb968e25b27df036a7bbd6faed
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-17 14:33:43 -0500

    tested date

--- __tests__/Article-test.js
+++ __tests__/Article-test.js
@@ -61,4 +61,28 @@ describe("Article", () => {
     let contentEl = TestUtils.findRenderedDOMComponentWithClass(articleRendered, "article__content")
     expect(contentEl.textContent).toBe(articleRendered.state.blogData.content)
   })
+
+  it("should have time stamp", () => {
+    let articleRendered = TestUtils.renderIntoDocument(
+      <Article params={"articleID: 123"}/>
+    )
+    // fake state for blogPost
+    articleRendered.setState({
+      blogData: {
+        _id: "1738",
+        author: "Fetty",
+        timeStamp: "2016.09.16",
+        content: "Lorem",
+        title: "moms spaghetti",
+        user_info: {
+          user_id: "679",
+          user_name: "remys boys",
+          user_imgSrc: "remyboys.jpeg"
+        }
+      }
+    })
+
+    let dateEl = TestUtils.findRenderedDOMComponentWithClass(articleRendered, "article__timestamp")
+    expect(dateEl.textContent).toBe(articleRendered.state.blogData.timeStamp)
+  })
 })
