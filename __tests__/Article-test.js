453e797818250849fba11a7d624779803abd68d4
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-17 14:24:43 -0500

    tested article content

--- __tests__/Article-test.js
+++ __tests__/Article-test.js
@@ -35,6 +35,30 @@ describe("Article", () => {
     })
 
     let titleEl = TestUtils.findRenderedDOMComponentWithClass(articleRendered, "article__contentTitle")
-    expect(titleEl.value).toBe("moms spaghetti")
+    expect(titleEl.textContent).toBe(articleRendered.state.blogData.title)
+  })
+
+  it("should have content", () => {
+    let articleRendered = TestUtils.renderIntoDocument(
+      <Article params={"articleID: 123"}/>
+    )
+    // fake state for blogPost
+    articleRendered.setState({
+      blogData: {
+        _id: "1738",
+        author: "Fetty",
+        timeStamp: "2016.09.16",
+        content: "Lorem",
+        title: "moms spaghetti",
+        user_info: {
+          user_id: "679",
+          user_name: "remys boys",
+          user_imgSrc: "remyboys.jpeg"
+        }
+      }
+    })
+
+    let contentEl = TestUtils.findRenderedDOMComponentWithClass(articleRendered, "article__content")
+    expect(contentEl.textContent).toBe(articleRendered.state.blogData.content)
   })
 })
